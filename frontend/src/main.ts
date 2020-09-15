39b5d8a507312ac025ad85d9c59c1ac196b06030
김지훈 <jh8039@naver.com>
2020-09-15 20:39:07 +0900

    merge

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './stores'
-<<<<<<< HEAD
-=======
-// import store from './vuex/store'
->>>>>>> 8361c26b317d2c24bbfa06dce77c17e73caba1f9
 
 Vue.config.productionTip = false
 
--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -1,11 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import Home from './Home'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules :{
-    Home,
-  }
-})
+  modules: {
+  },
+});
