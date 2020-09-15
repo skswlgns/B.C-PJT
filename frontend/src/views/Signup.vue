c68f05747403a1a0051f91f8ccab8ce58a7bf2a4
이다인 <dlekdls0213@naver.com>
2020-09-15 20:31:01 +0900

    S03P22B103-42 | 회원가입,로그인

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,16 +1,22 @@
 <template>
-	<div>
-		<div>
-			<label for="">아이디</label><input type="text" name="아이디">
+	<div class="singup-wrap">
+		<div class="signup-html">
+			<label for="">아이디</label>
+			<input type="text" name="아이디">
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
-	import Vue from 'vue'
-	export default Vue.extend({
-		
-	})
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+
+  },
+})
+
+export default class Home extends Vue {}
 </script>
 
 
