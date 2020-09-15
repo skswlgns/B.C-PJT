5b22df5aeedec7bfb8394f6238045131144e9504
김지훈 <jh8039@naver.com>
2020-09-15 11:35:18 +0000

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '@/views/Login.vue'
-import Signup from '@/views/Signup.vue'
 
 Vue.use(VueRouter)
 
