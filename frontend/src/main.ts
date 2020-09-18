6605c6712a41ccd11a11ee8d226b0feabfec0317
이다인 <dlekdls0213@naver.com>
2020-09-18 17:23:33 +0900

    wtf cookies

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './stores'
+import VueCookies from 'vue-cookies-ts'
 
+// export { CookiesOption, Cookies } from 'cookies-ts';
 
 Vue.config.productionTip = false
+Vue.use(VueCookies)
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+
+// Vue.cookies.config({expires: '7d'})
+// export default {
+//   install(_Vue: typeof Vue) {
+//     const vueCookies = new cookies();
+//     _Vue.prototype.$cookies = vueCookies;
+//     (_Vue as any).cookies = vueCookies;
+//   }
+// };
