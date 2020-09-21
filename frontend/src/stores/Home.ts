253f086b087e8aa8c555054d860b1b7ee6327cd4
김지훈 <jh8039@naver.com>
2020-09-21 09:32:20 +0900

    first init

---
+++ frontend/src/stores/Home.ts
@@ -0,0 +1,30 @@
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+
+@Module({namespaced: true})
+export default class Home extends VuexModule {
+
+  // states
+  public count: number = 0;
+
+  // getters
+  get doubledCount() {
+    return this.count * 2;
+  }
+
+  // mutations
+  @Mutation
+  public increment(delta: number) {
+    console.log(`increment mutation: ${delta}`);
+    this.count += delta;
+    console.log(this.count);
+  }
+
+  // actions
+  @Action({ commit: 'increment' })
+  public incr(delta: number) {
+    console.log(`increment action: ${delta}`);
+    delta ++;
+    return delta;
+  }
+
+}
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    {{ user_email }}
+    <p>{{count}} * 2 = {{doubledCount}}</p>
+    <button @click="incr(1)">incr</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+
+const CountStoreModule = namespace('Home');
+
+@Component
+export default class Home extends Vue {
+
+  @CountStoreModule.State('count')
+  private count!: number;
+
+  @CountStoreModule.Getter('doubledCount')
+  private doubledCount!: number;
+
+  @CountStoreModule.Action('incr')
+  private incr!: (delta: number) => void;
+
+}
+</script>
