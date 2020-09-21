5db1b037b270ce97b0c6bff7cab92f129aa7a9ac
김지훈 <jh8039@naver.com>
2020-09-21 17:46:53 +0900

    S03P23B103-4 | navbar & app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link>
-    </div> -->
-    <div class="flex">
-      <div class="center">
+  <v-app>
+    <div class="flex_pure">
+      <div class="center_pure">
         <img src="@/assets/images/logo.png" alt="로고" class="img_logo">
       </div>
       <div class="right">
+        <!-- 마이페이지 -->
         <span>
-          <i class="fas fa-user-circle fa-2x"></i>
+          <i class="fas fa-user-circle fa-2x" @click="goMypage()"></i>
         </span>
+        <!-- 알림 -->
         <span>
           <i class="far fa-bell fa-2x"></i>
         </span>
-        <span>로그아웃</span>
+        <!-- 로구아웃 -->
+        <span>
+          <i class="fas fa-sign-out-alt fa-2x"></i>
+        </span>
       </div>
     </div>
     <router-view/>
   </v-app>
 </template>
 
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    components: {
+
+    },
+
+    methods: {
+      goMypage() {
+        this.$router.push('/mypage')
+      }
+    }
+  })
+  export default class APP extends Vue {
+  }
+</script>
+
 <style lang="scss">
-  @import '@/assets/scss/app.scss';
+  @import '@/assets/scss/nav.scss';
 </style>
---
+++ frontend/src/views/MyPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cont10">
+
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    
+  })
+
+  export default class MyPage extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
