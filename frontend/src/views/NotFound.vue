547ca73ff9b65ac00f4e1220990eabb53517ed78
김지훈 <jh8039@naver.com>
2020-09-21 13:40:37 +0900

    NOT FOUND & APP.VUE

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <main class='container'>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>4</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <span class='particle'>0</span>
+      <article class='content'>
+        <p>Damnit stranger,</p>
+        <p>You got lost in the <strong>404</strong> galaxy.</p>
+        <p>
+          <button>Go back to earth.</button>
+        </p>
+      </article>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import '@/assets/scss/login.scss';
+
+@Component({
+    
+})
+
+export default class NotFound extends Vue {
+  
+}
+</script>
