251ca52538a6146cd5d25d56b0a26d7117fb46d5
김지훈 <jh8039@naver.com>
2020-09-22 13:52:37 +0900

    S03P23B103-16 | 뒷배경 변경

--- frontend/src/stores/Home.ts
+++ frontend/src/stores/Home.ts
@@ -2,11 +2,7 @@ import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
 
 @Module({namespaced: true})
 export default class Home extends VuexModule {
-<<<<<<< HEAD
   
-=======
-
->>>>>>> develop
   // states
   public count: number = 0;
 
--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="cont10">
-
+  <div>
+    <h1>마이페이지</h1>
+    <div class="user-box d-flex">
+      <img src="https://t1.daumcdn.net/cfile/tistory/991DA6445EFEB65E15" alt="" class="box">
+      <div class="pure-mt">
+        <span class="nick-size">닉네임</span>
+        <p>ㅁ 국가</p>
+        <span>ㅁ 잘하는 언어</span>
+        <span>| 
+          <v-responsive
+          class="text-center grey lighten-2 rounded-pill d-inline-flex align-center justify-center ma-1"
+          height="1.2vw"
+          width="5vw"
+          >
+            초고수
+          </v-responsive></span>
+      </div>
+      <div class="ml-auto my-auto mr-3">
+        <v-btn
+          color="primary"
+          class="ma-10">
+          통역가 신청하기
+        </v-btn>
+      </div>
+      <p>{{ windowWidth }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +32,14 @@
   import { Component, Vue } from 'vue-property-decorator';
 
   @Component({
-    
+
   })
 
   export default class MyPage extends Vue {
+
   }
 </script>
 
 <style lang="scss" scoped>
-
+  @import '@/assets/scss/mypage.scss'
 </style>
