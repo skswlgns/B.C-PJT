7a1495fa32cf631bd50f55c597dd15672332241c
yonguk Kim <emplam27@gmail.com>
2020-10-02 19:07:57 +0900

    Update Port Number

--- frontend/src/stores/UserPage.ts
+++ frontend/src/stores/UserPage.ts
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import axios from 'axios'
-import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
-import router from '@/router';
+import Vue from "vue"
+import axios from "axios"
+import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators"
+import router from "@/router"
 
-const SERVER_URL = 'https://j3b103.p.ssafy.io/api'
+const SERVER_URL = "https://j3b103.p.ssafy.io/api"
 
-@Module({namespaced: true})
+@Module({ namespaced: true })
 export default class UserPage extends VuexModule {
-
   // states
-  public userinfo: any = {};
+  public userinfo: any = {}
 
   // getters
   // get doubledCount() {
@@ -18,31 +17,27 @@ export default class UserPage extends VuexModule {
 
   // mutations
   @Mutation
-  public async saveuserinfo(temp_data:any = {}) {
+  public async saveuserinfo(temp_data: any = {}) {
     console.log(temp_data)
     this.userinfo = temp_data
   }
 
   // actions
-  @Action({ commit: 'saveuserinfo' })
+  @Action({ commit: "saveuserinfo" })
   public async get_userpage(temp: string) {
-      if (Vue.cookies.isKey('token')) {
-        const res = await axios.get(`${SERVER_URL}/users/${temp}`)
-        if (res.data.user_email === Vue.cookies.get('email')) {
-          router.push('/mypage')
-        } 
-        else {
-          return res.data
-        }
+    if (Vue.cookies.isKey("token")) {
+      const res = await axios.get(`${SERVER_URL}/users/${temp}`)
+      if (res.data.user_email === Vue.cookies.get("email")) {
+        router.push("/mypage")
+      } else {
+        return res.data
       }
-      else {
-        alert('로그인을 해주세요!')
-        router.push('/login')
-      }
+    } else {
+      alert("로그인을 해주세요!")
+      router.push("/login")
+    }
   }
 
   @Action
-  public async get_article() {
-      
-  }
+  public async get_article() {}
 }
